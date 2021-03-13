ae7ee6de066281fdc309a531c1ee4fe4bddda427
Edgar Delgado <jose.delgado@avaldigitallabs.com>
2021-03-13 18:51:06 -0500

    Se mejora la manera de leer de bucket

--- app.mjs
+++ app.mjs
@@ -4,6 +4,7 @@ import express from "express";
 import utiles from "./src/node_local/services/UtilesSrv.mjs";
 import tuplaHandler from "./src/node_local/services/TuplaHandler.mjs";
 import adminHandler from "./src/node_local/services/AdminHandler.mjs";
+import storageHandler from "./src/node_local/services/StorageHandler.mjs";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use("/", express.static("src"));
 app.use("/api/utiles", utiles);
 app.use("/api/tup", tuplaHandler);
 app.use("/adm", adminHandler);
-
+app.use("/storage", storageHandler);
 
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
--- src/node_local/services/StorageHandler.mjs
+++ src/node_local/services/StorageHandler.mjs
@@ -1,47 +1,94 @@
 import storagePackage from "@google-cloud/storage";
+import express from "express";
+
 const { Storage } = storagePackage;
 const storageInstance = new Storage();
 
 const bucket = storageInstance.bucket("proyeccion-colombia1.appspot.com");
 
+var router = express.Router();
+
 /**
  * https://googleapis.dev/nodejs/storage/latest/File.html#download
  */
-
 export class StorageHandler {
+  static MIME_TYPE_TEXT = ["application/json", "text/plain", "text/html"];
+
+  static isMimeTypeText(metadata) {
+    return StorageHandler.MIME_TYPE_TEXT.indexOf(metadata.contentType) >= 0;
+  }
+
+  /**
+   * Lee el archivo como texto y asume que existe
+   * @param filePath
+   * @encoding ascii, utf8
+   */
+  static async readString(filePath, encoding = "utf8") {
+    return (await StorageHandler.readBinary(filePath)).toString(encoding);
+  }
+
+  /**
+   * Retorna el Buffer!
+   * @param filePath
+   */
+  static async readBinary(filePath) {
+    const file = bucket.file(filePath);
+    const contents = (await file.download())[0];
+    return contents;
+  }
+
   /**
    * Lee un archivo de texto plano y responde con su contenido y metadata
    * @param filePath
    */
-  static async get(filePath) {
+  static read(filePath, type="buffer", encoding="utf8") {
+    const file = bucket.file(filePath);
+    const metadataPromise = file.getMetadata();
+    let contentPromise;
+    if (type == "text") {
+      contentPromise = StorageHandler.readString(filePath, encoding);
+    } else {
+      contentPromise = StorageHandler.readBinary(filePath);
+    }
     return new Promise((resolve, reject) => {
-      const file = bucket.file(filePath);
-      file.exists().then(function (data) {
-        const exists = data[0];
-        if (exists) {
-          var archivo = file.createReadStream();
-          var buf = "";
-          archivo.on("data", function (d) {
-            buf += d;
+      Promise.all([metadataPromise, contentPromise]).then(
+        function (respuesta) {
+          const metadata = respuesta[0][0];
+          const content = respuesta[1];
+          resolve({
+            metadata: metadata,
+            data: content,
           });
-
-          file.getMetadata().then(function (data) {
-            const metadata = data[0];
-            archivo
-              .on("end", function () {
-                resolve({
-                  metadata: metadata,
-                  plainText: buf,
-                });
-              })
-              .on("error", function (err) {
-                reject(err);
-              });
-          });
-        } else {
+        },
+        function () {
           resolve(null);
         }
-      });
+      );
     });
   }
 }
+
+/**
+ * name=public/hola.txt&type=text&download
+ */
+router.get("/read", function (req, res) {
+  const key = req.query.name;
+  const downloadFlag = req.query.download;
+  let readPromise;
+  readPromise = StorageHandler.read(key, req.query.type, req.query.encoding);
+  readPromise.then(function (rta) {
+    if (rta != null) {
+      res.writeHead(200, {
+        "Content-Type": rta.metadata.contentType,
+        "Content-disposition":
+          downloadFlag != undefined ? "attachment;filename=" + key : "inline",
+        "Content-Length": rta.data.length,
+      });
+      res.end(rta.data);
+    } else {
+      res.status(202).end();
+    }
+  });
+});
+
+export default router;
