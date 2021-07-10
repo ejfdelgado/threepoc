bd3ccaa1025704cefcd3059c66c0515f3374f1c2
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2021-07-10 17:54:52 -0500

    Se verifica que la pagina publica tiene angular

--- src/1/html/cv/js/index-public.mjs
+++ src/1/html/cv/js/index-public.mjs
@@ -1,1 +1,17 @@
-console.log("hey esto se ve muy bien!");
+import { dataContactMeForm } from "../../../../js/front/angular/directives/dataContactMeForm.mjs";
+import { mailService } from "../../../../js/front/angular/services/mailService.mjs";
+import { safeHTML } from "../../../../js/front/angular/filters/safeHtml.mjs";
+import { orderItem } from "../../../../js/front/angular/filters/orderItem.mjs";
+
+export const Tuto3Module = angular
+  .module("message", [])
+  .filter("safeHtml", safeHTML)
+  .filter("orderItem", orderItem)
+  .directive("publicContactMeForm", dataContactMeForm)
+  .service("mailService", mailService).name;
+
+export const AppModule = angular.module("app", [Tuto3Module]).name;
+
+$(document).ready(function () {
+  angular.bootstrap(document, ["app"]);
+});
